10efe5672f0b844e70e35fb648d9f4825e878fce
Kavya Subramanian <kavyasub@unc.edu>
2023-02-12 00:47:25 -0500

    Home page + Form page functional and looks decent

--- frontend/meal-pal/src/routes/Home.js
+++ frontend/meal-pal/src/routes/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import '../components/App.css';
+// import companyLogo from './src/logo.png';
+
 function Home() {
     const handleClick = () => {
         window.location.href = "forms";
     }
 
     return (
-      <header>
-        <p>
-          Weekly meal plans curated just for you.
-        </p>
+      <header className="App-header">
+        {/* <img src={"frontend/src/logo.png"} alt="React Image" />
+        <img src={companyLogo} alt="didn't work"/> */}
+        <div>
+            <img src="/logo2.jpg" alt="Meal Pal Inc. logo"/>
+        </div>
+        <div>
+            <h1 class="otherWords">
+              Weekly Meal Plans Curated Just For You.
+            </h1>
+        </div>
+        
+      
         <div>
           <button type="button" class = "button" onClick = {handleClick}> <h4 class = "button-font">GET STARTED</h4></button>
         </div>
