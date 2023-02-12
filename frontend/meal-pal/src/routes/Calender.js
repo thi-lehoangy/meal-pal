73503ff79242524677063c1ee1942cfee497f238
Kavya Subramanian <kavyasub@unc.edu>
2023-02-12 01:26:06 -0500

    Test

---
+++ frontend/meal-pal/src/routes/Calender.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+
+function Calendar() {
+
+    const [posts, setPosts] = useState([]);
+ 
+    useEffect(() => {
+      const fetchPost = async () => {
+         const response = await fetch(
+            'http://localhost:3000/getItems'
+         );
+         const data = await response.json();
+         console.log(data);
+         setPosts(data);
+      };
+      fetchPost();
+   }, []);
+
+    return (
+        <div>
+            {posts.map(posts => <div>{posts.date}</div>)}
+        </div>
+    );
+};
+
+export default Calendar;
