da9121752d381595682909de527edf1c4db6c751
Thi Le <thi.le@unc.edu>
2023-02-12 10:53:07 -0500

    Revert "multiple calendars"
    This reverts commit 2fb9e42f059f34a4f162834dd7132d89c07a3f65.

--- frontend/meal-pal/src/index.js
+++ frontend/meal-pal/src/index.js
@@ -1,45 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter as Router,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from "./routes/Home";
-import Header from "./components/Header";
-import Forms from "./routes/Forms";
-import Calendar, { CalendarNavigation } from "./routes/Calendar";
+import Home from './routes/Home';
+import Header from './components/Header';
+import Forms from './routes/Forms';
+import Calendar from './routes/Calendar';
 
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+      path: '/',
+      element: <Home />
   },
   {
-    path: "header",
-    element: <Header />,
+      path: 'header',
+      element: <Header />
   },
   {
-    path: "forms",
-    element: <Forms />,
+    path: 'forms',
+    element: <Forms />
   },
   {
-    path: "calendar/:dateParam",
-    element: <Calendar />,
-  },
-  {
-    path: "calendar-nav",
-    element: <CalendarNavigation />
-  },
+    path: 'calendar',
+    element: <Calendar />
+  }
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router = { router } />
   </React.StrictMode>
 );
 
