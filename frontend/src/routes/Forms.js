58d6809dc3bd972e4995db39050459de79dfbdf1
Lasya Kambhampati <84462162+lasya02@users.noreply.github.com>
2023-02-11 23:25:04 -0500

    image header

---
+++ frontend/src/routes/Forms.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+function Forms() {
+  const handleClick = () => {
+    window.location.href = "/Forms";
+}
+    return (<div>
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <div>
+  <label>
+    What university do you attend?
+    <select>
+        <option value="UNC">UNC</option>
+        <option value="DUKE">DUKE</option>
+        <option value="NC STATE">NC STATE</option>
+      </select>
+  </label>
+
+  </div>
+
+  <div>
+  <label>
+    What is your current meal plan? 
+    <select>
+        <option value="BLOCK100">BLOCK100</option>
+        <option value="BLOCK120">BLOCK120</option>
+        <option value="BLOCK160">BLOCK160</option>
+        <option value="BLOCK200">BLOCK200</option>
+      </select>
+  </label>
+
+  </div>
+
+  <div>
+  <label>
+  To help us build a menu inclusive of your dietary needs, please indicate any allergies/dietary restrictions you have 
+    <select>
+        <option value="None">None</option>
+        <option value="Halal">Halal</option>
+        <option value="Vegetarian">Vegetarian</option>
+        <option value="Vegan">Vegan</option>
+        <option value="Nut Allergy">Nut Allergy</option>
+        <option value="Gluten-Free">Gluten-Free</option>
+        <option value="Dairy-Free">Dairy-Free</option>
+        <option value="Egg Allergy">Egg Allergy</option>
+      </select>
+  </label>
+
+  </div>
+
+  <div>
+          <button type="button" class = "button" onClick = {handleClick}> <h4 class = "button-font">GET STARTED</h4></button>
+        </div>
+
+
+ 
+
+  <input type="submit" value="Submit" />
+</form>
+    </div>
+    )
+}
+
+export default Forms;
---
+++ frontend/src/routes/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import companyLogo from './src/logo.png';
+
+function Home() {
+    const handleClick = () => {
+        window.location.href = "/";
+    }
+
+    return (
+      <header className="App-header">
+        {/* <img src={"frontend/src/logo.png"} alt="React Image" />
+        <img src={companyLogo} alt="didn't work"/> */}
+        <div class="adjust-line-height">
+            <img src="/logo2.jpg" alt="Meal Pal Inc. logo"/>
+        </div>
+        <div class="adjust-line-height">
+            <p id="otherWords">
+              Weekly Meal Plans Curated Just For You.
+            </p>
+        </div>
+        
+      
+       
+
+        <div>
+          <button type="button" class = "button" onClick = {handleClick}> <h4 class = "button-font">GET STARTED</h4></button>
+        </div>
+      </header>
+    )
+}
+
+export default Home;
