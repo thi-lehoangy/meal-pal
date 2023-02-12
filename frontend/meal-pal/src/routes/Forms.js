f08a688f3ee922f8e68a9bf51ef55b7c3394aaee
Kavya Subramanian <kavyasub@unc.edu>
2023-02-11 23:53:26 -0500

    Form complete with select components

--- frontend/meal-pal/src/routes/Forms.js
+++ frontend/meal-pal/src/routes/Forms.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './Forms.css';
+import Select from 'react-select'
 
 function Forms() {
   const handleClick = () => {
@@ -9,57 +11,82 @@ function Forms() {
     console.log("Success")
   }
   
-  return (<div>
+  return (<div class = "form-page">
+    <h1> Generate your personalised meal plan!</h1>
         <form>
+    <div class = "form-item">
   <label>
-    Name:
+    Name: &nbsp;
     <input type="text" name="name" />
   </label>
-  <div>
-  <label>
-    What university do you attend?
-    <select>
-        <option value="UNC">UNC</option>
-        <option value="DUKE">DUKE</option>
-        <option value="NC STATE">NC STATE</option>
-      </select>
-  </label>
+  </div>
+
+  <div class = "form-item">
+    What university do you currently attend?
+    <br></br>
+    <br></br>
+  <Schools />
 
   </div>
 
-  <div>
-  <label>
+  <div class = "form-item">
     What is your current meal plan? 
-    <select>
-        <option value="BLOCK100">BLOCK100</option>
-        <option value="BLOCK120">BLOCK120</option>
-        <option value="BLOCK160">BLOCK160</option>
-        <option value="BLOCK200">BLOCK200</option>
-      </select>
-  </label>
-
+    <br></br>
+    <br></br>
+    <MealPlan />
   </div>
 
-  <div>
-  <label>
-  To help us build a menu inclusive of your dietary needs, please indicate any allergies/dietary restrictions you have 
-    <select>
-        <option value="None">None</option>
-        <option value="Halal">Halal</option>
-        <option value="Vegetarian">Vegetarian</option>
-        <option value="Vegan">Vegan</option>
-        <option value="Local">Local</option>
-        <option value="Organic">Organic</option>
-        <option value="Smart-Choice">Smart Choice</option>
-      </select>
-  </label>
-
+  <div class = "form-item">
+    To help us build a menu inclusive of your dietary needs, please indicate any allergies/dietary restrictions you have.
+    <br></br>
+    <br></br>
+    <DietNeeds />
   </div>
 
-    <button class = "button" type="submit" value="Submit" onSubmit = {handleSubmit}> Submit </button>
+    <button class = "button" type="submit" value="Submit" onSubmit = {handleSubmit}> <h4 class = "button-font">SUBMIT</h4> </button>
     </form>
     </div>
     )
 }
 
+const options = [
+  { value: 'unc', label: 'University of North Carolina' },
+  { value: 'ncsu', label: 'North Carolina State University' },
+  { value: 'duke', label: 'Duke University' }
+]
+
+const dietOptions = [
+  { value: 'none', label: 'None' },
+  { value: 'halal', label: 'Halal' },
+  { value: 'vegetarian', label: 'Vegetarian' },
+  { value: 'vegan', label: 'Vegan' },
+  { value: 'local', label: 'Local' },
+  { value: 'organic', label: 'Organic' },
+  { value: 'smart-choice', label: 'Smart Choice'},
+]
+
+const options2 = [
+  { value: 'block100', label: 'Block 100' },
+  { value: 'block120', label: 'Block 120' },
+  { value: 'block160', label: 'Block 160' },
+  { value: 'block200', label: 'Block 200' },
+]
+const Schools = () => (
+  <Select options={options} />
+)
+
+
+const MealPlan = () => (
+  <Select options={options2} />
+)
+
+const DietNeeds = () => (
+  <Select
+  defaultValue={dietOptions[0]}
+  isMulti
+  options={dietOptions}
+  className="basic-multi-select"
+  classNamePrefix="select"
+/>
+)
 export default Forms;
