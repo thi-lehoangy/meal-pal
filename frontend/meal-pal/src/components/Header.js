003d44fe048059409232b7aa8fc04b95cb0c92e8
Kavya Subramanian <kavyasub@unc.edu>
2023-02-11 20:04:19 -0500

    Website Restructured

---
+++ frontend/meal-pal/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return <header>
+        <div className="header-main">
+            <div className="header-text">              
+            </div>
+        </div>
+    </header>
+}
+
+export default Header;
